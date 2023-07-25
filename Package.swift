48b5a30067f09a11d32287040a82e872fef1dd40
Gleb Kovalenko <yourpendos@gmail.com>
2023-07-25 13:57:08 +0300

    Added package

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Codex",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_15),
+        .watchOS(.v3),
+        .tvOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "Codex",
+            targets: ["Codex"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Codex",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "CodexTests",
+            dependencies: ["Codex"]),
+    ]
+)
